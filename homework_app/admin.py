a91bd0856e6c9250ba5a03dab6ae56c2a8eb3f31
Roshan <np03cs4a 220211@heraldcollege.edu.np>
2024-05-20 23:28:08 +0545

    Fixded issue and bug

---
+++ homework_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Subject)
+admin.site.register(Homework)
+admin.site.register(HomeworkReview)
+admin.site.register(SubjectTeacher)
